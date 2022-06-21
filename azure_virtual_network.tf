4ab942fedabb78eabf888dcc2ac5bf3ca647c78b
R1999T <64471333+R1999T@users.noreply.github.com>
2022-06-21 17:23:13 +0530

    azure_security_group_update

--- azure_virtual_network.tf
+++ azure_virtual_network.tf
@@ -34,3 +34,38 @@ resource "azurerm_virtual_network" "rt_vn" {
     environment = "dev"
   }
 }
+
+//subnet
+resource "azurerm_subnet" "rt_subnet" {
+  name                 = "rt1_subnet"
+  resource_group_name  = azurerm_resource_group.rt_rg.name
+  virtual_network_name = azurerm_virtual_network.rt_vn.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+//azure security group
+resource "azurerm_network_security_group" "rt_sg" {
+  name                = "rt-sg"
+  location            = azurerm_resource_group.rt_rg.location
+  resource_group_name = azurerm_resource_group.rt_rg.name
+
+  tags = {
+    environment = "dev"
+  }
+
+}
+//now comes the rules
+resource "azurerm_network_security_rule" "rt_dev_rule" {
+  name                        = "rt_dev_rule"
+  priority                    = 100
+  direction                   = "Inbound" //to allow access to our servers
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*" //add your IP address to provide a particular server to access this(try with AWS) 
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rt_rg.name
+  network_security_group_name = azurerm_network_security_group.rt_sg.name
+}
+
